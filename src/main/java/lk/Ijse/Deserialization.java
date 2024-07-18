7d6fca05ea72ef9c8dac7c1efc897e207cb28686
jayawickrma <142202862+jayawickrma@users.noreply.github.com>
2024-07-18 23:13:10 +0530

    add lombok and created student details

---
+++ src/main/java/lk/Ijse/Deserialization.java
@@ -0,0 +1,4 @@
+package lk.Ijse;
+
+public class Deserialization {
+}
---
+++ src/main/java/lk/Ijse/Studnt.java
@@ -0,0 +1,14 @@
+package lk.Ijse;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Studnt {
+    String id;
+    String name;
+    String age;
+}
