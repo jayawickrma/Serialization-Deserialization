900010c672491a483084de8d5f2d820b948ccae6
jayawickrma <142202862+jayawickrma@users.noreply.github.com>
2024-07-18 23:29:18 +0530

    remove lombok

--- src/main/java/lk/Ijse/Studnt.java
+++ src/main/java/lk/Ijse/Studnt.java
@@ -2,13 +2,48 @@ package lk.Ijse;
 
 import lombok.*;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
-@Data
+
 public class Studnt {
     String id;
     String name;
     String age;
+
+    public Studnt(String id, String name, String age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Studnt{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", age='" + age + '\'' +
+                '}';
+    }
 }
