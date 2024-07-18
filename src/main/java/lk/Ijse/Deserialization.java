e411996fe167663c7df66aa2d64b78e185f8616c
jayawickrma <142202862+jayawickrma@users.noreply.github.com>
2024-07-19 00:00:30 +0530

    Deserialization

--- src/main/java/lk/Ijse/Deserialization.java
+++ src/main/java/lk/Ijse/Deserialization.java
@@ -6,12 +6,19 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 
 public class Deserialization {
-    Studnt studnt = new Studnt("1", "Nishan", "kalutara");
-    public static void main(String[] args) throws ClassNotFoundException {
-        try(ObjectInputStream objectInputStream =new ObjectInputStream(new FileInputStream("nishan"))){
-            Studnt desirialization =(Studnt)objectInputStream.readObject();
-            System.out.println("deserialization "+desirialization);
+
+    public static void main(String[] args) {
+        Studnt studnt =null;
+        try(ObjectInputStream objectInputStream =new ObjectInputStream(new FileInputStream("nisha"))){
+            studnt =(Studnt)objectInputStream.readObject();
+            System.out.println("deserialization "+studnt);
+
+        }
+        catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
         } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
     }
